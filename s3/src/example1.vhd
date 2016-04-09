bd5bb5b3b0d9056dc209d53f8b82ae381d5a97ac
Iman Tabrizian <tabrizian@outlook.com>
2016-04-09 17:31:27 +0430

    Minor improvement.

--- s3/src/example1.vhd
+++ s3/src/example1.vhd
@@ -5,10 +5,15 @@ use IEEE.numeric_std.all;
 entity example1 is
     end example1;
 architecture behavorial of example1 is
-    signal A, B, C, Y, Z : integer;
+    signal A : integer := 4;
+    signal B : integer := 4;
+    signal C : integer := 4;
+    signal Y : integer := 4;
+    signal Z : integer := 4;
 begin
     process(A, B, C)
-        variable M,N : integer;
+        variable M : integer := 4;
+        variable N : integer := 4;
     begin
         M := A;
         N := B;
