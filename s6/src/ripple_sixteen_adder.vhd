9de3aaf0f77cfbaae2e4593e04af5bdaa3b9d9a1
Iman Tabrizian <tabrizian@outlook.com>
2016-05-07 17:46:25 +0430

    Adds subtractor.

--- s6/src/ripple_sixteen_adder.vhd
+++ s6/src/ripple_sixteen_adder.vhd
@@ -1,14 +1,13 @@
 LIBRARY IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity ripple_sixteen_adder is
     port (A,B:in STD_LOGIC_VECTOR(15 downto 0);
           sum:out STD_LOGIC_VECTOR(15 downto 0);
-          cin:in STD_LOGIC;
           cout:out STD_LOGIC;
           command:in STD_LOGIC);
 end ripple_sixteen_adder;
-
 
 architecture gate_level_implementation of ripple_sixteen_adder is
     component full_adder is
@@ -17,13 +16,19 @@ architecture gate_level_implementation of ripple_sixteen_adder is
     end component;
 
     signal cout_mid : STD_LOGIC_VECTOR(15 downto 0);
+    signal C : STD_LOGIC_VECTOR(15 downto 0);
+    signal Cin : STD_LOGIC;
 begin
+    C <= B when command = '0' else
+         not B when command = '1';
+
+    Cin <= '1' when command = '1' else
+           '0' when command = '0';
+
     FA1: full_adder port map(A=>A(0), B=>B(0), Cin=>Cin, sum=>sum(0), cout=>cout_mid(0));
-      
-    GEN_REG: 
-   for I in 1 to 15 generate
-      FA : full_adder port map(A=>A(I), B=>B(I), Cin=>cout_mid(I-1), sum=>sum(I), cout=>cout_mid(I));
-   end generate GEN_REG;  
-      
-      
+
+    GEN_REG: for I in 1 to 15 generate
+        FA : full_adder port map(A=>A(I), B=>B(I), Cin=>cout_mid(I-1), sum=>sum(I), cout=>cout_mid(I));
+    end generate GEN_REG;
+
 end gate_level_implementation;
