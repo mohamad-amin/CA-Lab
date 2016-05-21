6b5d494ed1225166fc342a07c70f59d4374e60ac
Mohammad Mahdi Samiei <mohmahsamiei@gmail.com>
2016-05-21 17:32:56 +0430

    samiei again adds :|

--- s6/src/mux.vhd
+++ s6/src/mux.vhd
@@ -2,14 +2,14 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity mux is
+entity mux_sixteen is
     port(A : in STD_LOGIC_VECTOR(15 downto 0);
          B : in STD_LOGIC_VECTOR(15 downto 0);
     s :in STD_LOGIC;
 	O : out STD_LOGIC_VECTOR(15 downto 0));
-end mux;
+end mux_sixteen;
 
-architecture gate_level of mux is
+architecture gate_level of mux_sixteen is
 begin
 		O <= A when s = '0' else 
 		B when s = '1';
