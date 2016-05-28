1bab20981994e6b126ffa71f322b3a07335ea371
Iman Tabrizian <tabrizian@outlook.com>
2016-05-28 16:59:11 +0430

    Improves alu.

--- s6/src/alu.vhd
+++ s6/src/alu.vhd
@@ -57,10 +57,12 @@ architecture behavioral of alu is
     end component;
     component ripple_sixteen_adder is
         port (
-        A,B:in STD_LOGIC_VECTOR(15 downto 0);
-        sum:out STD_LOGIC_VECTOR(15 downto 0);
-        cout:out STD_LOGIC;
-        command:in STD_LOGIC);
+                 A:in STD_LOGIC_VECTOR(15 downto 0);
+                 B:in STD_LOGIC_VECTOR(15 downto 0);
+                 sum:out STD_LOGIC_VECTOR(15 downto 0);
+                 cout:out STD_LOGIC;
+                 command:in STD_LOGIC
+             );
     end component;
     component xnor_eight is
         port(
@@ -72,11 +74,14 @@ architecture behavioral of alu is
     component xor_eight is
         port(
                 A : in STD_LOGIC_VECTOR(15 downto 0);
-             B : in STD_LOGIC_VECTOR(15 downto 0);
-             xors :out STD_LOGIC_VECTOR(15 downto 0)
-         );
+                B : in STD_LOGIC_VECTOR(15 downto 0);
+                xors :out STD_LOGIC_VECTOR(15 downto 0)
+            );
     end component;
+
     signal sums, xors, xnors, ors, ands, multiplies, dividers, shrs, shls : STD_LOGIC_VECTOR(15 downto 0);
+    signal command_in : STD_LOGIC;
+    signal cout : STD_LOGIC;
 begin
     and_er : and_eight port map(a, b, ands);
     or_er : or_sixteen port map(a, b, ors);
@@ -84,4 +89,18 @@ begin
     xnor_er : xnor_eight port map(a, b, xnors);
     shr_er : shr_sixteen port map(a, '0', shrs);
     shl_er : shl_sixteen port map(a, '0', shls);
+    multipl_er : multiplier_eight port map(a, b, multiplies);
+    divid_er : divider_sixteen port map(a, b, dividers);
+    full_add_er : ripple_sixteen_adder port map(a, b, sums, cout, command_in);
+
+    res <= ands when command = "0000" else
+           ors when command = "0001" else
+           xors when command = "0010" else
+           xnors  when command = "0011" else
+           shrs  when command = "0100" else
+           shls when command = "0101" else
+           multiplies when command = "0110" else
+           dividers when command = "0111" else
+           sums when command = "1000";
+
 end behavioral;
