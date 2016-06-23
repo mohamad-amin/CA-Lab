339577e4be641d12416360e6710c6bf4cdeb6aa4
Iman Tabrizian <tabrizian@outlook.com>
2016-06-24 02:30:41 +0430

    Improves pc.

--- s6/src/pc.vhd
+++ s6/src/pc.vhd
@@ -5,6 +5,8 @@ use IEEE.numeric_std.all;
 entity pc is
     port(
             increment : in STD_LOGIC;
+            load : in STD_LOGIC;
+            in_addr : in STD_LOGIC_VECTOR(3 downto 0);
             pc_out : out STD_LOGIC_VECTOR(3 downto 0)
         );
 end entity;
@@ -13,10 +15,11 @@ architecture behavorial of pc is
     signal current_pc : STD_LOGIC_VECTOR(3 downto 0) := "0000";
     signal temp : integer;
 begin
+    pc_out <= in_addr when load = '1';
     process(increment)
     begin
-        if(increment'event) then
-            temp <= to_integer(unsigned(current_pc)) + 1;
+        if(increment'event and load = '0') then
+            temp <= to_integer(unsigned(current_pc)) + 2;
             current_pc <= STD_LOGIC_VECTOR(to_unsigned(temp, 4));
         end if;
     end process;
